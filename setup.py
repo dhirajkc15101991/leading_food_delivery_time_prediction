629b80bf278c07438284c3db476ca4b5cb9844db
dhirajkc15101991 <dhirajkc15101991@gmail.com>
2025-04-03 18:56:52 +0530

    Initail Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Build Ml-Project for predicting the delivery time of order',
+    author='Dhiraj_Kumar',
+    license='',
+)
